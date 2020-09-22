21c7d72fd9849d5766b6e8c5a2234395b41de6d4
LastWalkerB <jawadmustafa571@gmail.com>
2020-09-23 01:29:11 +0500

    LOL

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls'))
+    path('polls/', include('polls.urls')),
+    path('hello/', include('hello.urls')),
+    path('', TemplateView.as_view(template_name='home/main.html')),
 ]
